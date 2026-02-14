3d37912937959559fc3995437eacfcb406b01a05
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-02-14 17:41:25 +0530

    feat(backend): generate unique UUIDs for users to track distinct sessions

--- backend/index.js
+++ backend/index.js
@@ -1,128 +1,143 @@
-import {WebSocketServer} from 'ws'
-import dotenv from 'dotenv'
-dotenv.config()
-const socketsServer= new WebSocketServer({port:process.env.PORT})
+import { WebSocketServer } from "ws";
+import dotenv from "dotenv";
+import crypto from "crypto";
 
-const allConnected = []
+dotenv.config();
+const socketsServer = new WebSocketServer({ port: process.env.PORT });
 
-socketsServer.on('connection', function(socket){
+const allConnected = [];
 
-    socket.onmessage= (e)=>{
-       const msg= JSON.parse(e.data)
-       if(msg.type=="join"){
+socketsServer.on("connection", function (socket) {
+  socket.onmessage = (e) => {
+    const msg = JSON.parse(e.data);
+    if (msg.type == "join") {
+      const index = allConnected.findIndex((ele) => ele.socket == socket);
+      if (index != -1) {
+        allConnected.splice(index, 1);
+      }
 
-        const index= allConnected.findIndex((ele)=> ele.socket==socket)
-        if(index!=-1){
-            allConnected.splice(index,1)
+      const userId = crypto.randomUUID();
+
+      allConnected.push({
+        socket: socket,
+        roomId: msg.payload.roomId,
+        username: msg.payload.username,
+        userId: userId,
+      });
+
+      const currentRoomId = msg.payload.roomId;
+
+      let usersCount = 0;
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId) {
+          usersCount++;
         }
+      }
 
-            allConnected.push({
-                socket: socket,
-                roomId: msg.payload.roomId,
-                username: msg.payload.username
-            })
+      socket.send(
+        JSON.stringify({
+          sender: "System",
+          message: "init",
+          yourUserId: userId,
+          usersCount: usersCount,
+        }),
+      );
 
-             const currentRoomId=msg.payload.roomId
-            
-                let usersCount=0;
-            for(let i=0;i<allConnected.length;i++){
-                if(allConnected[i].roomId==currentRoomId){
-                    usersCount++;
-                }
-            }
-                 for(let i=0;i<allConnected.length;i++){
-                if(allConnected[i].roomId==currentRoomId){
-                    allConnected[i].socket.send(JSON.stringify({
-                        sender: 'System',
-                        message: 'join',
-                        username: msg.payload.username,
-                        timestamp: new Date(),
-                        usersCount: usersCount
-                    }))
-                }
-            }
-
-       }
-       if(msg.type=="chat"){
-
-    const currentRoomId= msg.payload.roomId
-
-        for(let i=0;i<allConnected.length;i++){
-            if(allConnected[i].roomId==currentRoomId){
-              allConnected[i].socket.send(JSON.stringify({
-                sender: msg.payload.username,
-                text: msg.payload.message,
-                image: msg.payload.image,
-                timestamp: new Date()
-              }))
-            }
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId && allConnected[i].socket !== socket) {
+          allConnected[i].socket.send(
+            JSON.stringify({
+              sender: "System",
+              message: "join",
+              username: msg.payload.username,
+              userId: userId,
+              timestamp: new Date(),
+              usersCount: usersCount,
+            }),
+          );
         }
-
-       }
-        if(msg.type=="leave"){
-            const findIndex= allConnected.findIndex((ele)=>ele.socket==socket)
-        allConnected.splice(findIndex,1)
-        
-       const currentRoomId=msg.payload.roomId
-                let usersCount=0;
-            for(let i=0;i<allConnected.length;i++){
-                if(allConnected[i].roomId==currentRoomId){
-                    usersCount++;
-                }
-            }
-
-             for(let i=0;i<allConnected.length;i++){
-                if(allConnected[i].roomId==currentRoomId){
-                    allConnected[i].socket.send(JSON.stringify({
-                        sender: 'System',
-                        message: 'leave',
-                        username: msg.payload.username,
-                        timestamp: new Date(),
-                        usersCount: usersCount
-                    }))
-                }
-            }
-
-        }
-
+      }
     }
+    if (msg.type == "chat") {
+      const currentRoomId = msg.payload.roomId;
 
-    socket.onclose= ()=>{
-            const findIndex= allConnected.findIndex((ele)=>ele.socket==socket)
-            if(findIndex==-1){
-                return
-            }
-            const currentSocket=allConnected[findIndex]
-        allConnected.splice(findIndex,1)
-       
-           let currentRoomId=null
-            if(currentSocket){
-                currentRoomId=currentSocket.roomId
-            }
-      if(currentRoomId){
-                let usersCount=0;
-            for(let i=0;i<allConnected.length;i++){
-                if(allConnected[i].roomId==currentRoomId){
-                    usersCount++;
-                }
-            }
-        
-
-             for(let i=0;i<allConnected.length;i++){
-                if(allConnected[i].roomId==currentRoomId){
-                    allConnected[i].socket.send(JSON.stringify({
-                        sender: 'System',
-                        message: 'leave',
-                        username: currentSocket.username,
-                        timestamp: new Date(),
-                        usersCount: usersCount
-                    }))
-                }
-            }
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId) {
+          allConnected[i].socket.send(
+            JSON.stringify({
+              sender: msg.payload.username,
+              senderId: msg.payload.userId,
+              text: msg.payload.message,
+              image: msg.payload.image,
+              timestamp: new Date(),
+            }),
+          );
         }
-
+      }
     }
+    if (msg.type == "leave") {
+      const findIndex = allConnected.findIndex((ele) => ele.socket == socket);
+      allConnected.splice(findIndex, 1);
 
+      const currentRoomId = msg.payload.roomId;
+      let usersCount = 0;
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId) {
+          usersCount++;
+        }
+      }
 
-})
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId) {
+          allConnected[i].socket.send(
+            JSON.stringify({
+              sender: "System",
+              message: "leave",
+              username: msg.payload.username,
+              userId: msg.payload.userId,
+              timestamp: new Date(),
+              usersCount: usersCount,
+            }),
+          );
+        }
+      }
+    }
+  };
 
+  socket.onclose = () => {
+    const findIndex = allConnected.findIndex((ele) => ele.socket == socket);
+    if (findIndex == -1) {
+      return;
+    }
+    const currentSocket = allConnected[findIndex];
+    allConnected.splice(findIndex, 1);
+
+    let currentRoomId = null;
+    if (currentSocket) {
+      currentRoomId = currentSocket.roomId;
+    }
+    if (currentRoomId) {
+      let usersCount = 0;
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId) {
+          usersCount++;
+        }
+      }
+
+      for (let i = 0; i < allConnected.length; i++) {
+        if (allConnected[i].roomId == currentRoomId) {
+          allConnected[i].socket.send(
+            JSON.stringify({
+              sender: "System",
+              message: "leave",
+              username: currentSocket.username,
+              userId: currentSocket.userId,
+              timestamp: new Date(),
+              usersCount: usersCount,
+            }),
+          );
+        }
+      }
+    }
+  };
+});
